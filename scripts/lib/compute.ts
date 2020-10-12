4df2862bbe08c169d5f8b98f1458f8ae0238bf46
aggre <hiroyuki.aggre@gmail.com>
2020-10-12 15:18:54 +0900

    change file structure

---
+++ scripts/lib/compute.ts
@@ -0,0 +1,106 @@
+import stakingRecords from '../../data/staking.json'
+import {BigNumber} from 'ethers'
+import {toBigNumber} from './number'
+import {BASIS, HOLDERS_SHARE} from './constants'
+import {sortByBlockNumber} from './collection'
+
+type Rewards = {
+	transactionHash: string
+	blockNumber: number
+	action: string
+	hookedUser: string
+	hookedProperty: string
+	rewards: {
+		total: string
+		interestPrice: string
+		holdersPrice: string
+		holdersPriceForLastWithdrawnReward: string
+		cumulativeHoldersAmountPerProperty: string
+	}
+}
+
+const zero = toBigNumber(0)
+
+const storage: Map<
+	| 'LastCumulativeReward'
+	| 'LastCumulativeHoldersRewardPrice'
+	| 'LastCumulativeInterestPrice',
+	BigNumber
+> = new Map()
+
+const lastCumulativeHoldersRewardsPerProperty: Map<
+	string,
+	{
+		amount: BigNumber
+		price: BigNumber
+	}
+> = new Map()
+
+export const compute = (records: typeof stakingRecords): Rewards[] =>
+	sortByBlockNumber(records).map(
+		(record): Rewards => {
+			const action = record._action
+			const transactionHash = record.transactionHash
+			const blockNumber = record.blockNumber
+			const hookedUser = record._transaction.from
+			const hookedProperty = record._property
+			const totalStaked = toBigNumber(record._totalStakedLast)
+			const propertyStaked = record._thePropertyStakedLast
+			const totalReward = toBigNumber(record._cumulativeTotalRewards)
+
+			// Get last values from storage
+			const lastCReward = storage.get('LastCumulativeReward') ?? zero // Previous value of `mTotal`
+			const lastCHoldersReward =
+				storage.get('LastCumulativeHoldersRewardPrice') ?? zero // Previous value of `holdersPrice`
+			const lastCInterest = storage.get('LastCumulativeInterestPrice') ?? zero // Previous value of `interestPrice`
+
+			// Compute latest global values
+			const mTotal = totalReward.mul(BASIS)
+			const price = totalStaked.isZero()
+				? zero
+				: mTotal.sub(lastCReward).div(totalStaked)
+			const holdersShare = price.mul(HOLDERS_SHARE).div(100)
+			const holdersPrice = holdersShare.add(lastCHoldersReward)
+			const interestPrice = price.sub(holdersShare).add(lastCInterest)
+
+			// Compute latest values about the Property
+			const lastCHoldersPProperty = lastCumulativeHoldersRewardsPerProperty.get(
+				hookedProperty
+			)
+			const cumulativeHoldersAmountPerProperty = holdersPrice
+				.sub(lastCHoldersPProperty?.price ?? zero)
+				.mul(propertyStaked)
+				.add(lastCHoldersPProperty?.amount ?? zero)
+
+			// Store computed latest values
+			if (action === 'stake' || action === 'unstake') {
+				storage.set('LastCumulativeReward', mTotal)
+				storage.set('LastCumulativeHoldersRewardPrice', holdersPrice)
+				storage.set('LastCumulativeInterestPrice', interestPrice)
+				lastCumulativeHoldersRewardsPerProperty.set(hookedProperty, {
+					amount: cumulativeHoldersAmountPerProperty,
+					price: holdersPrice,
+				})
+			}
+
+			const holdersPriceForLastWithdrawnReward = holdersPrice
+				.sub(lastCHoldersPProperty?.price ?? zero)
+				.mul(record._thePropertyStaked)
+				.add(lastCHoldersPProperty?.amount ?? zero)
+
+			return {
+				transactionHash,
+				blockNumber,
+				action,
+				hookedUser,
+				hookedProperty,
+				rewards: {
+					total: mTotal.toString(),
+					interestPrice: interestPrice.toString(),
+					holdersPrice: holdersPrice.toString(),
+					holdersPriceForLastWithdrawnReward: holdersPriceForLastWithdrawnReward.toString(),
+					cumulativeHoldersAmountPerProperty: cumulativeHoldersAmountPerProperty.toString(),
+				},
+			}
+		}
+	)
---
+++ scripts/lib/dry.ts
@@ -0,0 +1,73 @@
+import computed from '../../data/computed.json'
+import {sortByBlockNumber} from './collection'
+import {IterableElement} from 'type-fest'
+
+const findLastTransactions = (
+	data: typeof computed,
+	createKey: (el: IterableElement<typeof data>) => string,
+	targetAction: string[]
+) => {
+	const last: Map<string, IterableElement<typeof data>> = new Map()
+	for (const iterator of data) {
+		const {action, blockNumber} = iterator
+		if (!targetAction.includes(action)) {
+			continue
+		}
+
+		const key = createKey(iterator)
+		const got = last.get(key)
+		if (got && blockNumber < got.blockNumber) {
+			continue
+		}
+
+		last.set(key, iterator)
+	}
+
+	return Array.from(last.values())
+}
+
+const shouldInitStakeOnProperty = (data: typeof computed) =>
+	findLastTransactions(
+		data,
+		(x) => `stake-or-unstake-${x.hookedProperty}${x.hookedUser}`,
+		['stake', 'unstake']
+	)
+
+const shouldInitLastStakeOnProperty = (data: typeof computed) =>
+	findLastTransactions(data, (x) => x.hookedProperty, ['stake', 'unstake'])
+
+const shouldInitLastStake = (data: typeof computed) =>
+	findLastTransactions(data, () => 'stake-or-unstake', ['stake', 'unstake'])
+
+const shouldinitLastWithdraw = (data: typeof computed) =>
+	findLastTransactions(
+		data,
+		(x) => `withdraw-${x.hookedProperty}${x.hookedUser}`,
+		['withdraw']
+	)
+
+export const dry = (
+	records: typeof computed
+): {
+	__initStakeOnProperty: typeof computed
+	__initLastStakeOnProperty: typeof computed
+	__initLastStake: typeof computed
+	__initLastWithdraw: typeof computed
+} =>
+	((sortedRecords) =>
+		(([
+			__initStakeOnProperty,
+			__initLastStakeOnProperty,
+			__initLastStake,
+			__initLastWithdraw,
+		]) => ({
+			__initStakeOnProperty,
+			__initLastStakeOnProperty,
+			__initLastStake,
+			__initLastWithdraw,
+		}))([
+			shouldInitStakeOnProperty(sortedRecords),
+			shouldInitLastStakeOnProperty(sortedRecords),
+			shouldInitLastStake(sortedRecords),
+			shouldinitLastWithdraw(sortedRecords),
+		]))(sortByBlockNumber(records))
